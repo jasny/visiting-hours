21e822a27cc6f891cb1a1b25894af2a8a5d69b02
Arnold Daniels <arnold@jasny.net>
2025-09-10 00:39:23 +0200

    Fix certbot installation for HTTPS

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -7,5 +7,9 @@ test -e /etc/httpd/conf.d/virtual-host.conf || cat <<EOT | sudo tee /etc/httpd/c
 </VirtualHost>
 EOT
 
-sudo certbot -n -d opkraambezoek.nl,www.opkraambezoek.nl --apache --agree-tos --email arnold@jasny.net
+if command -v certbot >/dev/null 2>&1; then
+  sudo certbot -n -d opkraambezoek.nl,www.opkraambezoek.nl --apache --redirect --agree-tos --email arnold@jasny.net
+else
+  echo "certbot not installed" >&2
+fi
 
